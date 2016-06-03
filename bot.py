f11d04e019ccfc83814a31b4a7e3ed88c1a5b114
evanpaul22 <evanpaul22@gmail.com>
2016-06-03 00:56:28 -0700

    Optimization & logs

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # A simple call-and-response bot
 # TO DO:
-# - Logs
+# - Detailed Logs
 # - Save phrase and reply to new section of config file
-# - Tweak sleep time & comment retrieval
-# - Log matches, don't recheck
 import praw
 import api_auth
 import time
 
-SLEEP_TIME = 300 # Time to sleep between searches in seconds
+SLEEP_TIME = 10 # Time to sleep between searches in seconds
 results = open("results.txt", "a")
 matches = 0
 # Parse saved API refresh token
@@ -54,11 +52,11 @@ disallowed = [
 checked = [] # Comments that have already been checked (shouldn't really be an issue with /r/all, but just to be sure)
 # Change these to fit your usecase
 target = "sneak peak"
-reply_string = "Sneak Peek*\n\nhttp://theoatmeal.com/comics/sneak_peek\n\n---\n*I am a bot, report any misfires or complaints via private message to /u/StealthSummit*"
-print "Searching for a sly, crafty mountain in /r/all comments..."
-
+reply_string = "Sneak peek*\n\nhttp://theoatmeal.com/comics/sneak_peek\n\n---\n*I am a bot, report any misfires or complaints via private message to /u/StealthSummit*"
+total = 0
 # Main loop
 while True:
+    print "Fetching..."
     count = 0
     comments = praw.helpers.flatten_tree(r.get_comments("all", sort = "new", limit = 1000)) # Grabs up to 1000 latest comments
     for comment in comments:
@@ -71,7 +69,13 @@ while True:
             comment.reply(reply_string)
         checked.append(comment.id)
         print comment.id,"..."
+    total += count
     print "Checked",count,"comments"
+    print matches, "matches so far"
+    print total, "comments checked this session"
+    # Flush buffer every so often
+    if len(checked) > 2400:
+        checked = []
     print "Sleeping for:",SLEEP_TIME,"seconds"
     time.sleep(SLEEP_TIME)
 
